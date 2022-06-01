654ca5a7f27ce5e5ee44f431b0873d09c7b9b2ea
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-06-01 13:32:04 -0500

    Edit the 3 files

--- data.sql
+++ data.sql
@@ -11,3 +11,27 @@ INSERT INTO Animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO Animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', '2005-06-12', -45, FALSE, 1);
 INSERT INTO Animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', '2005-06-07', 20.4, TRUE, 7);
 INSERT INTO Animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', '1998-10-13', 17, TRUE, 3);
+
+BEGIN;
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Josie Whittaker', 38);
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+COMMIT;
+SELECT * FROM owners;
+SELECT * FROM species;
+
+BEGIN;
+SELECT * FROM Animals;
+UPDATE Animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE Animals SET species_id = 1 WHERE species_id IS NULL;
+COMMIT;
+SELECT * FROM Animals;
+
+BEGIN;
+SELECT * FROM Animals;
+UPDATE Animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE Animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE Animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE Animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE Animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+COMMIT;
+SELECT * FROM Animals;
--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE Animals (
 );
 
 ALTER TABLE Animals ADD species VARCHAR(30);
+
+CREATE TABLE owners(
+  id INT PRIMARY KEY(id),
+  full_name VARCHAR(30),
+  age INT,
+);
+
+CREATE TABLE species(
+  id INT PRIMARY KEY(id),
+  name VARCHAR(30),
+);
+
+ALTER TABLE Animals DROP COLUMN species;
+ALTER TABLE Animals ADD species_id INT;
+ALTER TABLE Animals ADD owners_id INT;
+ALTER TABLE Animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+ALTER TABLE Animals ADD FOREIGN KEY (owners_id) REFERENCES owners (id);
